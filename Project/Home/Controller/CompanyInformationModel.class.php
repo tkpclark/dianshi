54a07ddfb8ae6f16771edca8d49d87b794e1f3e5
xuguishuang <37863124@qq.com>
2014-10-11 14:14:16 +0800

    xu_添加控制器

---
+++ Project/Home/Controller/CompanyInformationModel.class.php
@@ -0,0 +1,52 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+use Home\Model\ApplicantUserModel;
+class ApplicantUseController extends Controller {
+    public function Index(){
+
+		$this->assign('user','zhangsan');
+		$this->display();
+    }
+	/*
+	*
+	*注册
+	*
+	*/
+	public function Registration(){
+		$User = D('CompanyInformation');
+		$data['username'] = I('param.username','');
+		$data['password'] = I('param.password','','md5');
+		$data['verification_email'] = I('param.username','');
+		if (!$User->create($data)){
+			exit($User->getError());
+		}else{
+			if($id = $User->add()){
+				echo '注册成功,请到邮件激活';
+				//$_SERVER[HTTP_HOST]/index.php/a/31
+				$url = '你好, <b>朋友</b>! <br/>这是一封来自<a href="http://'.$_SERVER['HTTP_HOST'].'/thinkphp/index.php/a/'.$id.'" target="_blank">xxx网</a>的激活邮件！<br/>';
+				SendMail("$data[username]","邮件标题","$url");
+			}else{
+				$this->error('失败');
+			}
+		}
+	}
+	/*
+	*
+	*添加基本信息
+	*
+	*/
+	public function Applicant_Information(){
+		if(isset($_SESSION['username']) && $_SESSION['username']!=null){
+			$User = D('ApplicantUser');
+			$data['username'] = I('param.username','');
+			$data['password'] = I('param.password','','md5');
+			$data['verification_email'] = I('param.username','');
+
+		}else{
+			echo '请登录';
+		}
+		ECHO 1;
+	}
+	
+}
